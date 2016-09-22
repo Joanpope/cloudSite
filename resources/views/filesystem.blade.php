96507b2772e52154e294f1d4db6de1e775ac7b64
joanpope <joanpont19@gmail.com>
2016-09-22 14:02:11 +0200

    afegit el menu.

--- resources/views/filesystem.blade.php
+++ resources/views/filesystem.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 @section('content')
+<div id="itemMenu">
+  <ul class="list-unstyled">
+    <li id="down" class="option"><span class="glyphicon glyphicon-save-file">Descargar</span></li>
+    <li id="dele" class="option"><span class="glyphicon glyphicon-trash">Eliminar</span></li>
+    <li id="dele" class="option"><span class="glyphicon glyphicon-plus">Añadir categoria</span></li>
+  </ul>  
+</div>
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">FileSystem!</div>
                   <div id="dragandrophandler">
+
                     @yield("iconCreation")                    
                   </div>
             </div>
